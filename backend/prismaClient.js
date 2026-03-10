44d8cd066d4259089299a51489390ae758e2f80b
Vishnu7konda <Vishnu.konda40@gmail.com>
2026-03-11 00:01:12 +0530

    keys added again

--- backend/prismaClient.js
+++ backend/prismaClient.js
@@ -1,5 +1,15 @@
-import { PrismaClient } from '@prisma/client';
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
+const { PrismaClient } = require('@prisma/client');
 
-const prisma = new PrismaClient();
+import pg from 'pg';
+import { PrismaPg } from '@prisma/adapter-pg';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const pool = new pg.Pool({ connectionString: process.env.DATABASE_URL });
+const adapter = new PrismaPg(pool);
+
+const prisma = new PrismaClient({ adapter });
 
 export default prisma;
